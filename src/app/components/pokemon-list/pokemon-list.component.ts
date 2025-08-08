64b6e4d2ec880ef8fcd461dce1c0baf991e3d70b
Cesar Perea <cesarperea3@gmail.com>
2025-08-08 09:51:07 -0600

    feat: first view of PokemonList and visual changes

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -7,26 +7,107 @@ import {
 import { MatCardFooter, MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatListModule } from '@angular/material/list';
+import { Observable, ReplaySubject } from 'rxjs';
+import { AsyncPipe, CommonModule, TitleCasePipe } from '@angular/common';
+import { Pokemon } from '../../interfaces/pokemon-main.interface';
+import { ActivatedRoute, RouterLink, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-pokemon-list',
-  imports: [MatCardModule, MatCardFooter, MatListModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatCardFooter,
+    MatListModule,
+    MatChipsModule,
+    AsyncPipe,
+    TitleCasePipe,
+  ],
   templateUrl: './pokemon-list.component.html',
   styleUrl: './pokemon-list.component.scss',
 })
 export class PokemonListComponent implements OnInit {
-  private pokemonList: PokemonList[] = [];
-  constructor(private pokemonService: PokemonService) {}
+  unsubscribe$ = new ReplaySubject<void>();
+  public allTypes$: Observable<string[]>;
 
-  ngOnInit(): void {
-    // this.pokemonService.getList().subscribe({
-    //   next: (response: PokemonListResponse) => {
-    //     this.pokemonList = response.results;
-    //     console.log(this.pokemonList);
-    //   },
-    //   error: (error: any) => {
-    //     console.log(error);
-    //   },
-    // });
+  public pokemonList: PokemonList[] = [];
+  public pokemonCompleteList: Pokemon[] = [];
+  public completeListFiltered: Pokemon[] = [];
+  constructor(private pokemonService: PokemonService) {
+    this.allTypes$ = this.pokemonService.allTypes$;
+  }
+
+  ngOnInit() {
+    this.loadMainPokemons();
+    this.loadTypes();
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  loadMainPokemons() {
+    this.pokemonService.getList().subscribe({
+      next: (response: PokemonListResponse) => {
+        this.pokemonList = response.results;
+        this.pokemonList = this.shuffle(this.pokemonList);
+        this.loadPokemonData(this.pokemonList);
+      },
+      error: (error: any) => {
+        console.log(error);
+      },
+    });
+  }
+
+  loadTypes() {
+    this.pokemonService.getTypes().subscribe({
+      next: (response: any) => {},
+    });
+  }
+
+  loadPokemonData(pokemonList: PokemonList[]) {
+    pokemonList.forEach((pokemon) => {
+      this.pokemonService.getByName(pokemon.name).subscribe({
+        next: (response: Pokemon) => {
+          this.pokemonCompleteList.push(response);
+        },
+        error: (error: any) => {
+          console.log(error);
+        },
+      });
+      this.completeListFiltered = this.pokemonCompleteList;
+    });
+  }
+
+  filterByType(typeToFilter: string) {
+    console.log(typeToFilter);
+    switch (typeToFilter) {
+      case 'all':
+        this.completeListFiltered = this.pokemonCompleteList;
+        break;
+      default:
+        this.completeListFiltered = this.pokemonCompleteList.filter((pokemon) =>
+          pokemon.types.find((type) => type.type.name === typeToFilter)
+        );
+        break;
+    }
+  }
+  shuffle<T>(array: T[]): T[] {
+    const copy = [...array];
+    let currentIndex = copy.length;
+    let randomIndex: number;
+
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [copy[currentIndex], copy[randomIndex]] = [
+        copy[randomIndex],
+        copy[currentIndex],
+      ];
+    }
+
+    return copy;
   }
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { Pokemon } from '../interfaces/pokemon-main.interface';
 import { PokemonListResponse } from '../interfaces/pokemon-list.interface';
 import { DOCUMENT } from '@angular/common';
@@ -11,6 +11,12 @@ export class PokemonService {
   private baseUrl = 'https://pokeapi.co/api/v2/';
   private url = 'https://pokeapi.co/api/v2/pokemon/';
   private temporal = '?limit=100&offset=0';
+
+  private _allTypes = new BehaviorSubject<string[]>([]);
+
+  get allTypes$(): Observable<string[]> {
+    return this._allTypes.asObservable();
+  }
   constructor(private http: HttpClient) {}
 
   getList(): Observable<PokemonListResponse> {
@@ -21,5 +27,17 @@ export class PokemonService {
 
   getByName(name: string): Observable<Pokemon> {
     return this.http.get<Pokemon>(this.url + name);
+  }
+
+  getTypes(): Observable<PokemonListResponse> {
+    return this.http.get<PokemonListResponse>(this.baseUrl + 'type').pipe(
+      tap((res) => {
+        this._allTypes.next(res.results.map((type) => type.name));
+      })
+    );
+  }
+
+  getImage(url: string): Observable<Blob> {
+    return this.http.get(url, { responseType: 'blob' });
   }
 }
